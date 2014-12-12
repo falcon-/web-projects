a5de6fc716409dec785e39c9d1085ac57c2f2bde
Rafael Malave <rmalave@nest>
2014-12-11 19:54:46 -0800

    new views

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -6,13 +6,47 @@ from flask import Flask
 # Create the application object
 app = Flask(__name__)
 
+app.config['DEBUG'] = True
 # Use decorators to link the function to a url
+# Static route
 @app.route('/')
 @app.route('/hello')
-
 # define the view using a function
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!?!?!'
+
+
+# Dynamic route
+@app.route('/test/<search_query>')
+def search(search_query):
+    return search_query
+
+
+@app.route('/integer/<int:value>')
+def int_type(value):
+    print(value + 1)
+    return 'correct'
+
+
+@app.route('/float/<float:value>')
+def float_type(value):
+    print(value + 1)
+    return 'correct'
+
+
+# Dynamic value that accecpts slashes
+@app.route('/path/<path:value>')
+def path_type(value):
+    print(value)
+    return 'correct'
+
+@app.route('/name/<name>')
+def index(name):
+    if name.lower() == 'michael':
+        return 'Hello {}'.format(name), 200
+    else:
+        return 'Not Found', 404
+
 
 # start the development server by using the run() method
 if __name__ == '__main__':
